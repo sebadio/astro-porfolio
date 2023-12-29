5385f88f193881cd2cccdb79e1678ce769eeed7a
sebadio <sebaingame@gmail.com>
2023-12-29 15:33:01 -0300

    v1

---
+++ src/components/icons/index.js
@@ -0,0 +1,10 @@
+import Github from "./Github.astro";
+import Link from "./Link.astro";
+import LinkedIn from "./LinkedIn.astro";
+import Mail from "./Mail.astro";
+import Phone from "./Phone.astro";
+import Whatsapp from "./Whatsapp.astro";
+
+
+
+export { Github, Link, LinkedIn, Mail, Phone, Whatsapp };
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Nombre from "./Nombre.astro";
+import Proyecto from "./Proyecto.astro";
+import Contacto from "./Contacto.astro";
+import Imagen from "./Imagen.astro";
+import Curso from "./Curso.astro";
+import Tecnologias from "./Tecnologias.astro";
+
+
+export { Nombre, Proyecto, Contacto, Imagen, Curso, Tecnologias };
