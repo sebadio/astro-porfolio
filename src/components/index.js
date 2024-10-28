de6e7bb99bef098809fa04bdd804a521b14ef358
sebadio <sebastiandiazdopazo@gmail.com>
2024-10-28 20:04:40 -0300

    startin rewrite

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Nombre from "./Nombre.astro";
-import Proyecto from "./Proyecto.astro";
-import Contacto from "./Contacto.astro";
-import Imagen from "./Imagen.astro";
-import Curso from "./Curso.astro";
-import Tecnologias from "./Tecnologias.astro";
-
-
-export { Nombre, Proyecto, Contacto, Imagen, Curso, Tecnologias };
